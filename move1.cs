0c37a3accd8e6645dda4426d7d4dc27b2ac3d140
Raykoika <64694438+Raykoika@users.noreply.github.com>
2020-05-02 23:06:22 +0800

    Add files via upload

---
+++ move1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move1 : MonoBehaviour{
+    public Rigidbody2D tar;
+    public GameObject bullte;
+    void Start()
+    {
+        
+    }
+    void Update(){
+        
+        /*移動*/{if (Input.GetKey(KeyCode.A))
+            transform.position += new Vector3(-0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.Space)){
+            tar.AddForce(new Vector2(0,1),ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.J)){
+            Instantiate(bullte,transform.position,new Quaternion(0,0,0,0));
+        }
+        }
+        
+    }
+    void OnCollisionEnter2D(Collision2D Coll){
+        if(Coll.gameObject.tag == "DONTOUCHIT")
+            tar.AddForce(new Vector2(0,20),ForceMode2D.Impulse);
+            Coll.Rigidbody2D.AddForce(new Vector2(0,-5),ForceMode2D.Impulse);
+         
+    }
+}
---
+++ shoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0,1)*Time.deltaTime*5);
+        if (transform.position.y > 40)
+            Destroy(this.gameObject);
+    }
+}
